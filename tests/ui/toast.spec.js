959c19f3e98bdc39710070064376b9e8261d7ec9
Tommy <tommy@jhshuyuan.com>
2019-06-27 09:58:56 +0800

    toast添加测试用例

--- tests/ui/toast.spec.js
+++ tests/ui/toast.spec.js
@@ -1,10 +1,8 @@
-import { createWrapper, mount, shallowMount } from '@vue/test-utils'
+import { createWrapper } from '@vue/test-utils'
 import assert from 'assert'
 import Toast from 'web-ui/toast/src/toast.js'
-import TmToast from 'web-ui/toast/src/toast.vue'
 import { sleep } from '../helper'
 import sinon from 'sinon'
-import WorkBasic from '../components/basic.vue'
 
 describe('toast', () => {
     it('create', async () => {
@@ -45,6 +43,17 @@ describe('toast', () => {
         assert.strictEqual(vm.duration, 0)
     })
 
+    it('第一个参数为对象', async () => {
+        const message = 'message'
+        const vm = Toast({
+            message,
+        })
+        const wrapper = createWrapper(vm)
+        await sleep()
+        const wrapperContent = wrapper.find('.tm-toast-content')
+        assert.strictEqual(wrapperContent.text(), message)
+    })
+
     it('第二个参数为对象', () => {
         const icon = 'icon'
         const vm = Toast('hhh', {
@@ -62,5 +71,44 @@ describe('toast', () => {
         })
         assert.strictEqual(vm.icon, icon)
         assert.strictEqual(vm.duration, 1500)
+    })
+
+    it('destroy', async () => {
+        const onClose = sinon.fake()
+        const vm = Toast('destroy', 500, { onClose })
+        const wrapper = createWrapper(vm)
+        await sleep()
+        assert.strictEqual(wrapper.exists(), true)
+        await sleep(1000)
+        assert.strictEqual(wrapper.exists(), false)
+        assert.strictEqual(onClose.callCount, 1)
+    })
+
+    it('duration 为 0 不会销毁', async () => {
+        const onClose = sinon.fake()
+        const vm = Toast('destroy', 0, { onClose })
+        const wrapper = createWrapper(vm)
+        await sleep()
+        assert.strictEqual(wrapper.exists(), true)
+        await sleep(1000)
+        assert.strictEqual(wrapper.exists(), true)
+        assert.strictEqual(onClose.callCount, 0)
+    })
+
+    it('zindex递增', async () => {
+        const vm1 = Toast('first')
+        await sleep()
+        const vm2 = Toast('second')
+        await sleep()
+        assert.strictEqual(vm2.zIndex - vm1.zIndex, 1)
+    })
+
+    it('position', async () => {
+        const positions = ['top', 'middle', 'bottom']
+        positions.forEach(position => {
+            const vm = Toast('qq', { position })
+            const wrapper = createWrapper(vm)
+            assert.ok(wrapper.classes(`tm-toast-${position}`))
+        })
     })
 })
