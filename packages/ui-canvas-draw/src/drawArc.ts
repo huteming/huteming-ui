0c7f3fef085a80c78c21af737fcb13693b3e3fd6
Tommy <tommy@jhshuyuan.com>
2019-12-17 17:53:26 +0800

    style(canvas-draw): 删除 canvas-draw > drawArc 多余空格
    affects: @huteming/ui-canvas-draw

--- packages/ui-canvas-draw/src/drawArc.ts
+++ packages/ui-canvas-draw/src/drawArc.ts
@@ -3,18 +3,18 @@ import { ArcTypes } from '../types/enum'
 import CanvasDraw from './main'
 
 const defaults = {
-    startDegrees: 0, // 开始角度
-    endDegrees: 360, // 结束角度
-    direction: false, // 方向。false: 顺时针, true: 逆时针
-    lineWidth: 1,
-    shadowColor: '',
-    shadowOffsetX: 0,
-    shadowOffsetY: 0,
-    shadowBlur: 0,
-    color: '#fff', // 画笔颜色
-    fillColor: '', // 默认值为 color
-    strokeColor: '', // 默认值为 color
-    type: [ArcTypes.Stroke], // 闭合方式
+  startDegrees: 0, // 开始角度
+  endDegrees: 360, // 结束角度
+  direction: false, // 方向。false: 顺时针, true: 逆时针
+  lineWidth: 1,
+  shadowColor: '',
+  shadowOffsetX: 0,
+  shadowOffsetY: 0,
+  shadowBlur: 0,
+  color: '#fff', // 画笔颜色
+  fillColor: '', // 默认值为 color
+  strokeColor: '', // 默认值为 color
+  type: [ArcTypes.Stroke], // 闭合方式
 }
 /**
  * 绘画圆弧
@@ -26,83 +26,85 @@ const defaults = {
  * @returns {Object} options
  */
 export default function (this: CanvasDraw, x: number, y: number, r: number, options: DrawArcOption = {}): DrawArcConfig {
-    const config = formatOptions.call(this, x, y, r, options)
-    setContextConfig.call(this, config)
+  const config = formatOptions.call(this, x, y, r, options)
+  setContextConfig.call(this, config)
 
-    drawArc.call(this, config)
+  drawArc.call(this, config)
 
-    return config
+  return config
 }
 
 export function formatOptions (this: CanvasDraw, x: number, y: number, r: number, options: DrawArcOption): DrawArcConfig {
-    const { ratio } = this
-    const _x = (x + r) * ratio
-    const _y = (y + r) * ratio
-    const _r = r * ratio
+  const { ratio } = this
+  const _x = (x + r) * ratio
+  const _y = (y + r) * ratio
+  const _r = r * ratio
 
-    const config = Object.assign({ x: _x, y: _y, r: _r }, defaults, options)
+  const config = Object.assign({ x: _x, y: _y, r: _r }, defaults, options)
 
-    config.shadowOffsetX *= ratio
-    config.shadowOffsetY *= ratio
-    config.shadowBlur *= ratio
+  config.shadowOffsetX *= ratio
+  config.shadowOffsetY *= ratio
+  config.shadowBlur *= ratio
 
-    config.fillColor = config.fillColor || config.color
-    config.strokeColor = config.strokeColor || config.color
+  config.fillColor = config.fillColor || config.color
+  config.strokeColor = config.strokeColor || config.color
 
-    if (typeof config.type === 'string') {
-        config.type = [config.type]
-    }
-    if (config.type.length > 1) {
-        const _type = config.type.slice()
-        // 排序 => [closePath, stroke, fill]
-        _type.sort((prev, next) => {
-            if (next === 'closePath') {
-                return 1
-            }
-            if (prev !== 'closePath' && next === 'stroke') {
-                return 1
-            }
-            return -1
-        })
-        config.type = _type
-    }
+  if (typeof config.type === 'string') {
+    config.type = [config.type]
+  }
+  if (config.type.length > 1) {
+    const _type = config.type.slice()
+    // 排序 => [closePath, stroke, fill]
+    _type.sort((prev, next) => {
+      // istanbul ignore if
+      if (next === 'closePath') {
+        return 1
+      }
+      // istanbul ignore if
+      if (prev !== 'closePath' && next === 'stroke') {
+        return 1
+      }
+      return -1
+    })
+    config.type = _type
+  }
 
-    return config
+  return config
 }
 
 export function setContextConfig (this: CanvasDraw, config: DrawArcConfig): void {
-    const { context } = this
-    const { fillColor, strokeColor, shadowColor, shadowOffsetX, shadowOffsetY, shadowBlur, lineWidth } = config
+  const { context } = this
+  const { fillColor, strokeColor, shadowColor, shadowOffsetX, shadowOffsetY, shadowBlur, lineWidth } = config
 
-    context.fillStyle = fillColor
-    context.strokeStyle = strokeColor
-    context.lineWidth = lineWidth
-    context.shadowColor = shadowColor
-    context.shadowOffsetX = shadowOffsetX
-    context.shadowOffsetY = shadowOffsetY
-    context.shadowBlur = shadowBlur
+  context.fillStyle = fillColor
+  context.strokeStyle = strokeColor
+  context.lineWidth = lineWidth
+  context.shadowColor = shadowColor
+  context.shadowOffsetX = shadowOffsetX
+  context.shadowOffsetY = shadowOffsetY
+  context.shadowBlur = shadowBlur
 }
 
 export function drawArc (this: CanvasDraw, config: DrawArcConfig): void {
-    const { context } = this
-    const radians = Math.PI / 180
-    const {
-        x,
-        y,
-        r,
-        startDegrees,
-        endDegrees,
-        direction,
-        type,
-    } = config
+  const { context } = this
+  const radians = Math.PI / 180
+  const {
+    x,
+    y,
+    r,
+    startDegrees,
+    endDegrees,
+    direction,
+    type,
+  } = config
 
-    context.beginPath()
-    context.arc(x, y, r, radians * startDegrees, radians * endDegrees, direction)
-    type.forEach((item: ArcTypes) => {
-        if (typeof context[item] !== 'function') {
-            console.warn(`CanvasRenderingContext2D 上不存在该方法: ${item}`)
-            return
-        }
-        context[item]()
-    })
+  context.beginPath()
+  context.arc(x, y, r, radians * startDegrees, radians * endDegrees, direction)
+  type.forEach((item: ArcTypes) => {
+    if (typeof context[item] !== 'function') {
+      console.warn(`CanvasRenderingContext2D 上不存在该方法: ${item}`)
+      return
+    }
+    context[item]()
+  })
 }
--- packages/ui-request/src/formatOptions.ts
+++ packages/ui-request/src/formatOptions.ts
@@ -3,33 +3,33 @@ import { AxiosRequestConfig } from 'axios'
 import { isStandardBrowserEnv } from '@huteming/ui-tools/src/main'
 
 const mapHost = new Map([
-    ['fejh.jinghao.com', '//jhtest.jinghao.com'],
-    ['fejhdemo.jinghao.com', '//jhdemo.jinghao.com'],
-    ['jhsy.jinghao.com', '//api.jinghao.com'],
-    ['tommy.jinghao.com', '//api.jinghao.com']
+  ['fejh.jinghao.com', '//jhtest.jinghao.com'],
+  ['fejhdemo.jinghao.com', '//jhdemo.jinghao.com'],
+  ['jhsy.jinghao.com', '//api.jinghao.com'],
+  ['tommy.jinghao.com', '//api.jinghao.com']
 ])
 
 const defaults = {
-    baseURL: '',
-    timeout: 8000,
-    withCredentials: true,
-    jhsyAccountAlias: '',
-    jhsyRetry: 2,
-    jhsyRetryDelay: 1000,
-    jhsyRetryCount: 0,
+  baseURL: '',
+  timeout: 8000,
+  withCredentials: true,
+  jhsyAccountAlias: '',
+  jhsyRetry: 2,
+  jhsyRetryDelay: 1000,
+  jhsyRetryCount: 0,
 }
 const defaultHeaders = {
-    // 'Content-Type': 'application/x-www-form-urlencoded',
+  // 'Content-Type': 'application/x-www-form-urlencoded',
 }
 
 export default function (options: AxiosRequestConfig): RequestOptions {
-    const _options: RequestOptions = Object.assign({}, defaults, options, {
-        headers: Object.assign({}, defaultHeaders, options.headers),
-    })
-    /* istanbul ignore else */
-    if (!_options.baseURL && isStandardBrowserEnv()) {
-        _options.baseURL = mapHost.get(window.location.host) || '//jhtest.jinghao.com'
-    }
+  const _options: RequestOptions = Object.assign({}, defaults, options, {
+    headers: Object.assign({}, defaultHeaders, options.headers),
+  })
+  /* istanbul ignore else */
+  if (!_options.baseURL && isStandardBrowserEnv()) {
+    _options.baseURL = mapHost.get(window.location.host) || '//jhtest.jinghao.com'
+  }
 
-    return _options
+  return _options
 }
